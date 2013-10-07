89fafa5b722c1c88c60a9310213e0c6ccd8f81d1
Erik Rumppe <erumppe@gmail.com>
2013-10-07 14:33:18 -0700

    Re-refactored RSpec tests.  All test passing.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,35 +1,65 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-
   subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
 
   describe "Home page" do
     before { visit root_path }
+    let(:heading) { 'Welcome to the Microblog' }
+    let(:page_title) { '' }
 
-    it { should have_content('Microblog') }
-    it { should have_title(full_title('')) }
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Home')}
   end
 
   describe "Help page" do
     before { visit help_path }
+    let(:heading) { 'Help Page' }
+    let(:page_title) { 'Help' }
 
-    it { should have_content('Help Page') }
-    it { should have_title(full_title('Help')) }
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
+    let(:heading) { 'About Page' }
+    let(:page_title) { 'About' }
 
-    it { should have_content('About Page') }
-    it { should have_title(full_title('About')) }
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact' }
 
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
+    it_should_behave_like "all static pages"
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+
+    click_link "About"
+    expect(page).to have_title(full_title('About'))
+
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+
+    click_link "Home"
+    expect(page).to have_title(full_title(''))
+
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+
+    click_link "microblog"
+    expect(page).to have_title(full_title(''))
   end
 end
