cf8d9d3c7a3e7ff10468f9e6c7b653bf4f251528
Erik Rumppe <erumppe@gmail.com>
2013-10-07 12:58:00 -0700

    Named routes created.  Pages updated.  All RSpec tests passing.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,40 +3,57 @@ require 'spec_helper'
 describe "StaticPages" do
 
   describe "Home page" do
-  	it "Should have the content 'Sample App'" do
-  		visit '/static_pages/home'
-  		expect(page).to have_content('Sample App')
-  	end
-  	
-		it "Should have the title 'Home'" do
-			visit '/static_pages/home'
-			expect(page).not_to have_title('Home')
-		end
-  end
-  
-	describe "Help page" do
-		it "Should have the content 'Help'" do
-			visit '/static_pages/help'
-			expect(page).to have_content('Help')
-		end
-		
-  	it "Should have the title 'Help" do
-  		visit '/static_pages/help'
-  		expect(page).to have_title('Help')
-  	end
+    it "should have the h1 'Welcome to the Microblog'" do
+      visit root_path
+      expect(page).to have_content('Welcome to the Microblog')
+    end
+
+    it "should have the base title" do
+      visit root_path
+      expect(page).to have_title("Microblog")
+    end
+
+    it "should not have a custom page title" do
+      visit root_path
+      expect(page).not_to have_title('| Home')
+    end
   end
 
-	describe "About page" do
-		it "Should have the content 'About'" do
-			visit '/static_pages/about'
-			expect(page).to have_content('About')
-		end
-		
-  	it "Should have the title 'About'" do
-  		visit '/static_pages/about'
-  		expect(page).to have_title('About')
-  	end
+  describe "Help page" do
+    it "should have the h1 'Help'" do
+      visit help_path
+      expect(page).to have_content('Help Page')
+    end
+
+    it "should have the title 'Help'" do
+      visit help_path
+      expect(page).to have_title("Microblog | Help")
+    end
+
   end
 
-  
+  describe "About page" do
+    it "should have the h1 'About Page'" do
+      visit about_path
+      expect(page).to have_content('About Page')
+    end
+
+    it "should have the title 'About'" do
+      visit about_path
+      expect(page).to have_title("Microblog | About")
+    end
+
+  end
+
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit contact_path
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contact_path
+      expect(page).to have_title("Microblog | Contact")
+    end
+  end
 end
