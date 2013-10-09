de53d70650f9141cef754c27b4036cd290fb3dc9
Erik Rumppe <erumppe@gmail.com>
2013-10-09 09:00:49 -0700

    Added more RSpec testing per Ch 6 exercises.  All test passed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
 
+  has_secure_password
+
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, :email => true, uniqueness: { case_sensitive: false }
-  has_secure_password
   validates :password, length: { minimum: 6 }
 end
