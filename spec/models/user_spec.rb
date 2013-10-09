ee675e72ca928e4369d4f947c1c7a97fe926a909
Erik Rumppe <erumppe@gmail.com>
2013-10-09 08:35:59 -0700

    Added RSpec tests for password functions. All tests passed.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,10 @@ describe User do
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
@@ -55,5 +59,39 @@ describe User do
     end
 
     it { should_not be_valid }
+  end
+
+  describe "when password is not present" do
+    before { @user = User.new(name: "Example User", email: "user@example.com", password: "", password_confirmation: "")}
+
+    it { should_not be_valid }
+  end
+
+  describe "when passwords don't match" do
+    before { @user.password_confirmation = "mismatch" }
+
+    it { should_not be_valid }
+  end
+
+  describe "with a password that is too short" do
+    before { @user.password = "a" * 5 }
+
+    it { should be_invalid }
+  end
+
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by(email: @user.email) }
+
+    describe "with valid password" do
+      it { should eq found_user.authenticate(@user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not eq user_for_invalid_password }
+      specify { expect(user_for_invalid_password).to be_false }
+    end
   end
 end
