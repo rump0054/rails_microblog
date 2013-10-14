108a57d12d3ad499fbdd95aa63802d2b7f670745
Erik Rumppe <erumppe@gmail.com>
2013-10-14 12:50:41 -0700

    Signin logic completed. No signin tests written.  All other tests passing.  UI header menu working.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,23 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
+  before_create :create_remember_token
 
   has_secure_password
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, :email => true, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
