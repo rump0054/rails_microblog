ddab8ed5e0320e4a5339dac799d4a158110b23df
Erik Rumppe <erumppe@gmail.com>
2013-10-15 08:20:01 -0700

    Finished sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 class SessionsController < ApplicationController
   def new
-
   end
 
   def create
@@ -15,6 +14,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-
+    sign_out
+    redirect_to root_url
   end
 end
